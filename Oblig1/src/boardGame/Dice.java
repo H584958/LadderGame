c6a9d6368c24826af1ba9695f31531dc0ca0ddc2
H584958 <584958@stud.hvl.no>
2021-01-20 13:58:58 +0100

    Finished Javadoc in the Dice class.

--- Oblig1/src/boardGame/Dice.java
+++ Oblig1/src/boardGame/Dice.java
@@ -1,9 +1,18 @@
 package boardGame;
-
+/***
+ * Represents a
+ * @author Simon Kobbenes, Matias Vedeler, Christian Evensen
+ */
 public class Dice {
-
+    /***
+     * creates a dice.
+     */
     public Dice(){};
 
+    /***
+     * Gives a random number between 1-6.
+     * @return An integer between 1-6.
+     */
     public int rollDice() {
        return (int) (Math.random()*6) + 1;
     }
