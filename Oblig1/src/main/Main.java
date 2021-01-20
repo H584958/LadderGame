57ece785eb86bfc0aada55b4bcc1df5bf680d629
H584958 <584958@stud.hvl.no>
2021-01-20 14:06:10 +0100

    OK

--- Oblig1/src/main/Main.java
+++ Oblig1/src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
 import boardGame.Board;
-
+/***
+ * A Main to run the Board game.
+ * @author Simon Kobbenes, Matias Vedeler, Christian Evensen
+ */
 public class Main {
     public static void main(String[] args) {
         Board board = new Board(2);
